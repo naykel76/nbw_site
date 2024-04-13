2758d09113a09178f6ac6255e6efbae980edddd4
Nathan Watts <nathan@naykel.com.au>
2024-04-14 07:17:12 +1000

    install gotime

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Naykel\Gotime\RouteBuilder;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+*/
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.home');
+})->name('home');
+
+(new RouteBuilder('nav-main'))->create();
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+*/
+
+// (new RouteBuilder('nav-admin'))->create();
+
+// Route::middleware(['role:super|admin', 'auth'])->prefix('admin')->name('admin')->group(function () {
+//     Route::view('/', 'gotime::admin.dashboard'); // admin dashboard
+// });
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,45 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import purge from '@erbelion/vite-plugin-laravel-purgecss'
 
 export default defineConfig({
     plugins: [
+        // if there are issues, try with laravel.default
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/scss/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                }
+            },
+        },
+        purge({
+            paths: [
+                'resources/views/**/*.blade.php',
+                'vendor/naykel/**/resources/views/**/*.blade.php'
+            ],
+            safelist: {
+                standard: [/^\:has$/, /^\:is$/, /^\:not$/, /^\:where$/, /^\:disabled$/],
+                greedy: [/code$/, /hljs-/]
+            },
+            extractors: [
+                {
+                    extractor: (content) => {
+                        return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+                    },
+                    extensions: ['css', 'html', 'vue', 'php'],
+                },
+            ],
+        })
     ],
 });
