b00e864225e069434be6cbc7ef096364e6718125
Nathan Watts <nathan@naykel.com.au>
2025-11-05 15:49:21 +1000

    remove widget

--- tests/Feature/Livewire/WidgetCreateEditTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Livewire\WidgetCreateEdit;
-use Livewire\Livewire;
-
-it('renders successfully', function () {
-    Livewire::test(WidgetCreateEdit::class)
-        ->assertStatus(200);
-});
